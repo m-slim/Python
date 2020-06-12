dd6890f13e825f55cfb6fbb387815fe52500f319
Muhammad Bin Saleem <muhammad.saleem@gmail.com>
2020-06-13 00:48:25 +0500

    Aan do bhai lecture 2

--- Lec1/Lec1c/lec1c.py
+++ Lec1/Lec1c/lec1c.py
@@ -1,4 +1,13 @@
 pi = 3.142
+radius = 4
+area = pi * (radius**2)
+print (area)
+
+
 radius = 2
-area = 2 * pi * radius**2
+
+print (area)
+
+area = pi * (radius**2)
+
 print (area)
---
+++ Lec2/Lec2c/lec2c.py
@@ -0,0 +1,1 @@
+print ("Hello i'm the code in lecture 2 - python <3")
