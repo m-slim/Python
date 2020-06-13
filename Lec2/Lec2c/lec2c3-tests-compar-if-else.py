d499b2fa5a6a687784ba51a872a9db294facf773
Muhammad Bin Saleem <muhammad.saleem@gmail.com>
2020-06-13 14:05:31 +0500

    Changed code file names to include what we learned in the code

---
+++ Lec2/Lec2c/lec2c3-tests-compar-if-else.py
@@ -0,0 +1,14 @@
+# if <condition>:
+#     <expression>
+#     <expression>
+
+text = int(input("Enter text"))
+num = int (input ("Enter num"))
+
+if num>=text:
+    out=text + num
+    print ("num is greater than text and result is ", out)
+
+else:
+    print ("num is less than text")
+
