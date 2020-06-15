f1f15420095a838d4c1d1101a99bdbe1d4acb863
Muhammad Bin Saleem <muhammad.saleem@gmail.com>
2020-06-15 18:26:52 +0500

    A new code in lec2c3 as nested if else

--- Lec2/Lec2c/lec2c3-tests-compar-if-else-loops.py
+++ Lec2/Lec2c/lec2c3-tests-compar-if-else-loops.py
@@ -5,10 +5,24 @@
 text = int(input("Enter text"))
 num = int (input ("Enter num"))
 
+#if num>=text:
+#    out=text + num
+#    print ("num is greater than text and result is ", out)
+
+#else:
+#    print ("num is less than text")
+
+# Lets try nested if statements
+
 if num>=text:
-    out=text + num
-    print ("num is greater than text and result is ", out)
+    print ("num is greater than or equal to text")
+    if num>text:
+        out = num - text
+        print ("Num is greater than text therefore the difference is", out)
+    else:
+        out = num - text
+        print ("Both numbers are equal so the difference is", out)
 
 else:
-    print ("num is less than text")
-
+    out = text - num
+    print ("num is less than text so the difference between text and num is", out)
